29a12aff7a63a81b336a16c9174af12ea0d9a37a
bliitzkrieg <lucamatthewdecaprio@gmail.com>
2020-02-02 15:39:58 -0500

    Added routes to client and stripped useless client code

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
+import ListReports from './containers/ListReports';
+import ViewReport from './containers/ViewReport';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <ListReports />
+          </Route>
+          <Route path="/report/:id">
+            <ViewReport />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ client/src/containers/ListReports.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>List all reports</div>
+  );
+};
