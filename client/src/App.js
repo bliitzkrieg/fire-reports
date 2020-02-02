bb29c4ae3b14176d8be2f8ec05ceb47b5f9ebe78
bliitzkrieg <lucamatthewdecaprio@gmail.com>
2020-02-02 16:55:18 -0500

    Implemented listReports component, removed unnessessary files and added some basic styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 import Header from './components/Header';
-import ListReports from './components/ListReport';
+import ListReports from './components/ListReports';
 import ViewReport from './components/ViewReport';
 
 import './App.css';
---
+++ client/src/components/ListReports/index.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+
+import './ListReports.css';
+
+function useReports() {
+  const [reports, setReports] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/reports`);
+        const reports = await response.json();
+        setReports(reports);
+      } catch (err) {
+        console.error(err);
+        setReports([]);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return reports
+}
+
+export default () => {
+  const reports = useReports();
+
+  if (reports.length === 0) {
+    return "Fetching...";
+  }
+
+  return (
+    <div className="reports">
+      { reports.map(report => (
+        <div className="report" key={report.description.incident_number}>
+          <Link to={`/report/${report.description.incident_number}`}>
+            {report.description.incident_number}
+          </Link>
+        <div>{report.address.address_line1}, {report.address.city}</div>
+        <div>{report.description.subtype} - {report.description.type} </div>
+        <div>{report.fire_department.name}</div>
+        <div>Weather: {report.weather.currently.summary}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
