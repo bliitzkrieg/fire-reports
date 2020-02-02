a175c9994e78e934fdeb0d267eecd3d0b2ebcc92
bliitzkrieg <lucamatthewdecaprio@gmail.com>
2020-02-02 18:02:42 -0500

    Changed header to Link and moved inside React Router component

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Header />
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/">
             <ListReports />
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 import './Header.css';
 
 export default () => {
   return (
-    <header>
-      <img src='/logo.jpg' alt="Fire Reports Logo" />
-      <h1>Fire Reports</h1>
-    </header>
+    <Link to="/">
+      <header>
+        <img src='/logo.jpg' alt="Fire Reports Logo" />
+        <h1>Fire Reports</h1>
+      </header>
+    </Link>
   );
 };
